a65c6fc5bbcc3564dade3ead43b1b47704da7463
Your Name <brandon69shen@gmail.com>
2018-06-29 14:53:30 -0700

    added red component

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -8,7 +8,8 @@ import { Component } from '@angular/core';
  */
 @Component({
   selector: 'footer',
-  templateUrl: 'footer.html'
+  templateUrl: 'footer.html',
+  styleUrls: ['./footer.scss']
 })
 export class FooterComponent {
 
