eaf9fb81067c76a4ac32bf00ea8a22739a9b0857
Your Name <brandon69shen@gmail.com>
2018-07-02 11:00:08 -0700

    fixed all merge conflicts

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-<<<<<<< HEAD
-import { HomePage} from '../home/home';
-=======
-import { FormsPage } from '../forms/forms';
-import { HomePage } from '../home/home';
 
->>>>>>> ec738427ba81b13b9326ae1838ecb1e4955c736c
 /**
  * Generated class for the FeedPage page.
  *
@@ -23,20 +17,5 @@ export class FeedPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-  goToHomePage(){
-    this.navCtrl.setRoot(HomePage);
-
-  }
-
-<<<<<<< HEAD
-
-=======
-  goToFormsPage(){
-    this.navCtrl.setRoot(FormsPage);
-  }
-
-  goToHomePage(){
-    this.navCtrl.setRoot(HomePage);
-  }
->>>>>>> ec738427ba81b13b9326ae1838ecb1e4955c736c
+ 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { FeedPage } from '../feed/feed';
-import { FormsPage } from '../forms/forms';
 
 @Component({
   selector: 'page-home',
@@ -12,16 +10,4 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
     console.log("Home page instantiated");
   }
-
-  goToFeedPage(){
-    this.navCtrl.setRoot(FeedPage);
-  }
-  goToFormsPage(){
-    this.navCtrl.setRoot(FormsPage);
-  }
-
-  goToHomePage(){
-    this.navCtrl.setRoot(HomePage);
-  }
-
 }
