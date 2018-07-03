e32b5d4830e8ef84d93fc43780d3bbaf2b2475fc
carlc9 <ccheng2142@gmail.com>
2018-07-03 13:47:01 -0700

    scroll error

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer';
 import { RedComponent } from './red/red';
 import { ScrollComponent } from './scroll/scroll';
+import { ScrollnewComponent } from './scrollnew/scrollnew';
 @NgModule({
 	declarations: [FooterComponent,
     RedComponent,
-    ScrollComponent],
+    ScrollComponent,
+    ScrollnewComponent],
 	imports: [],
 	exports: [FooterComponent,
     RedComponent,
-    ScrollComponent]
+    ScrollComponent,
+    ScrollnewComponent]
 })
 export class ComponentsModule {}
