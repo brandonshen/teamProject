51944b322c49bac806d889a6b60e3f7014ea4558
Eric <eric.andrew.lu@gmail.com>
2018-07-10 09:36:34 -0700

    sliding bar

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -11,10 +11,32 @@ import { Component, Input } from '@angular/core';
   templateUrl: 'header.html'
 })
 export class HeaderComponent {
+  public menuIsVisible: boolean;
   @Input() headerTitle : string;
 
   constructor() {
     console.log('Hello HeaderComponent Component'); 
+    this.menuIsVisible = false;
+    // document.getElementById("mySidenav").style.width="0px";
+  }
 
+  // openNav() {
+  //   document.getElementById("mySidenav").style.width= "250px";
+  // }
+
+  // closeNav() {
+  //   document.getElementById("mySidenav").style.width= "0px";
+  // }
+
+  toggleMenu() {
+    if (this.menuIsVisible == false) {
+      document.getElementById("mySidenav").style.width="250px";
+      this.menuIsVisible = true;
+      console.log("Menu toggle on");
+    } else {
+      document.getElementById("mySidenav").style.width="0px";
+      this.menuIsVisible = false;
+      console.log("Menu toggle off");
+    }
   }
 }
